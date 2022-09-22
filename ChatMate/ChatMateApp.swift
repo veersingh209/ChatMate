3b18cc8dc8331c671afa4ef304a64e65b6c20e4d
Veer Singh <veersingh209@icloud.com>
2022-09-22 14:57:47 -0700

    Initial Commit

---
+++ ChatMate/ChatMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMateApp.swift
+//  ChatMate
+//
+//  Created by Veer Singh on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
