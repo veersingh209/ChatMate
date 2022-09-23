42e7748a8ec02150e9bd7f172bba0722dbd24817
Veer Singh <veersingh209@icloud.com>
2022-09-23 09:19:03 -0700

    Firebase setup

---
+++ ChatMate/ViewModel/AuthViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AuthViewModel.swift
+//  ChatMate
+//
+//  Created by Veer Singh on 9/23/22.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthViewModel {
+    
+    static func isUserLoggedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    static func getLoggedInUserId() -> String {
+        return Auth.auth().currentUser?.uid ?? ""
+    }
+    
+    static func logout() {
+        try? Auth.auth().signOut()
+    }
+    
+}
